a25e2f7b9919280ac92a023ba8099e53aeffa0ad
Kedar Kandel <kandelkedar07@gmail.com>
2022-04-04 02:34:46 +0300

    make it better

---
+++ frontend/src/components/LoginComponent/Login.jsx
@@ -0,0 +1,63 @@
+import "./login.css";
+
+import { useRef, useState } from "react";
+import PinDropIcon from "@mui/icons-material/PinDrop";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+import axios from "axios";
+
+const Login = ({ setShowLogin, myStorage, setCurrentUser }) => {
+  const [failure, setFailure] = useState(false);
+  const nameRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = {
+      username: nameRef.current.value,
+      password: passwordRef.current.value,
+    };
+    try {
+      const res = await axios.post("api/users/login", user);
+      myStorage.setItem("user", res.data.username);
+      setCurrentUser(res.data.username);
+      setShowLogin(false);
+      setFailure(false);
+    } catch (err) {
+      setFailure(true);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="logo">
+        <PinDropIcon className="pin-drop" />
+        <h4>KedPin</h4>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="username"
+          type="text"
+          placeholder="Username"
+          ref={nameRef}
+        />
+
+        <input
+          className="password"
+          type="password"
+          placeholder="password"
+          ref={passwordRef}
+        />
+        <button className="login-btn">Login</button>
+
+        {failure && <span className="failure"> Something went wrong!</span>}
+      </form>
+      <CancelIcon
+        className="cancel-login"
+        onClick={() => setShowLogin(false)}
+      />
+    </div>
+  );
+};
+
+export default Login;
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require('cors')
+const app = express();
+const path = require("path");
+
+app.use(cors());
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("Mongo db connected")
+}).catch((err)=>console.log(err));
+
+app.use("/api/pins", pinRoute);
+app.use("/api/users",userRoute);
+
+// server static assets if in production
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("frontend/build"));
+
+    app.get("*", (req, res)=>{
+        res.sendFile()
+    })
+}
+
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+// });
+
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Backend server is running");
+});
