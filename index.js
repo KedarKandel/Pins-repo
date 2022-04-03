dd6e89806a90328cb30747929380e5597eace9ee
Kedar Kandel <kandelkedar07@gmail.com>
2022-04-04 02:55:15 +0300

    version 1.0.0

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 const path = require("path");
 
@@ -11,21 +11,24 @@ const userRoute = require("./routes/users");
 dotenv.config();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log("Mongo db connected")
-}).catch((err)=>console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Mongo db connected");
+  })
+  .catch((err) => console.log(err));
 
 app.use("/api/pins", pinRoute);
-app.use("/api/users",userRoute);
+app.use("/api/users", userRoute);
 
 // server static assets if in production
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("frontend/build"));
+if (process.env.NODE_ENV === "production") {
+    app.use('frontend/public', express.static(path.join(__dirname, '/')));
 
-    app.get("*", (req, res)=>{
-        res.sendFile()
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/public", "index.html"));
+  });
 }
 
 // app.use(express.static(path.join(__dirname, "/frontend/build")));
@@ -34,7 +37,6 @@ if(process.env.NODE_ENV === "production"){
 //   res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
 // });
 
-
-app.listen(process.env.PORT || 5000, ()=>{
-    console.log("Backend server is running");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running");
 });
