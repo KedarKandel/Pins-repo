f90ee0317f9410269e3764b7967556fcc136c1ee
Kedar Kandel <kandelkedar07@gmail.com>
2022-04-04 03:38:32 +0300

    version 2

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.use("/api/users", userRoute);
 // server static assets if in production
 
 if (process.env.NODE_ENV === "production") {
-    app.use('frontend/public', express.static(path.join(__dirname, '/')));
+   app.use(express.static("frontend/build"))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 // app.use(express.static('./client/dist/'));
