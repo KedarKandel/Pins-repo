f377015bf98ab972b69c697464f98c0bf859c922
Kedar Kandel <kandelkedar07@gmail.com>
2022-04-04 23:21:43 +0300

    commmit

--- index.js
+++ index.js
@@ -23,19 +23,11 @@ app.use("/api/users", userRoute);
 
 // server static assets if in production
 
-// if (process.env.NODE_ENV === "production") {
-//    app.use(express.static("frontend/build"))
+if(process.env.NODE_ENV ==="prduction"){
+  app.use(express.static("frontend/build"));
+  
+}
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// }
-// app.use(express.static('./frontend/dist/'));
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
-});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running");
